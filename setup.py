f57e4fa3e806c57a300a9f3992f44f0d5b119e24
josie <latinue@gmail.com>
2024-05-07 15:21:23 -0400

    updated setup.py file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Josie",
+    author_email="fetinue3@gmail.com",
+    packages=find_packages()
+)
